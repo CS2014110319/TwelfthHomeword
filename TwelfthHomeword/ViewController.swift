c19458458f195f7b622a6f2e45dec9d3b0f80290
student <student@imac-04.local>
2018-12-08 10:23:15 +0800

    Initial Commit

---
+++ TwelfthHomeword/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwelfthHomeword
+//
+//  Created by student on 2018/12/8.
+//  Copyright © 2018年 lishunli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
