98048eb67e2e69712602ec025bafce731f8a06d6
student <student@iMac-04.local>
2018-12-18 11:19:27 +0800

    first commit

---
+++ TwelfthHomeword/DelViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DelViewController.swift
+//  TwelfthHomeword
+//
+//  Created by student on 2018/12/8.
+//  Copyright © 2018年 lishunli. All rights reserved.
+//
+
+import UIKit
+
+class DelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
